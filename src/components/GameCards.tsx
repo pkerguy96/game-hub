9ad6160596a48408efaca8c9143b64ffad5ca1a2
pkerguy96 <aymenelkor@hotmail.com>
2023-07-04 21:42:45 +0100

    platforms icons

---
+++ src/components/GameCards.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card, Image, CardBody, Heading, Text } from "@chakra-ui/react";
+
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+interface Props {
+  game: Game;
+}
+const GameCards = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCards;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,23 @@
-
-import {Text} from '@chakra-ui/react'
-import useGames from '../hooks/useGames'
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCards from "./GameCards";
 
 const GameGrid = () => {
-  const {games,error}= useGames();
+  const { games, error } = useGames();
   return (
     <>
-   { error &&  <Text>{error}</Text>}
-    <ul>
-       {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {games.map((game) => (
+          <GameCards key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
