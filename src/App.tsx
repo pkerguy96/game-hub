054efc1d38298ac3a85188a0e138ccb0baeb0a8c
pkerguy96 <aymenelkor@hotmail.com>
2023-07-04 23:33:08 +0100

    genre fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import {Grid,GridItem,Show } from '@chakra-ui/react'
-import Navbar from './components/Navbar'
-import GameGrid from './components/GameGrid'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
- return <Grid templateAreas={{ 
-  base: `"nav" "main"`,
-  lg: `"nav nav" "aside main"`
-  }}>
-   <GridItem area='nav' ><Navbar/></GridItem>
-   <Show above='lg'>
-   <GridItem area='aside' >aside</GridItem>
-   </Show>
-   <GridItem area='main'><GameGrid/></GridItem>
-   
- </Grid>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 const GameCards = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} width="300px" overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
